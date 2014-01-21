46e19675e59ead53a420b09a11340262d6d2e8ef
tq-jappy <tq-ed@v7.com>
2014-01-21 21:29:41 +0900

    move dblclicked item into other.

--- js/list-shuffle-main.js
+++ js/list-shuffle-main.js
@@ -8,11 +8,13 @@ function($, ko) {
 
   function ViewModel() {
     this.leftItems = ko.observableArray([
-        new Item("Item1", 111),
-        new Item("Item2", 222),
-        new Item("Item3", 333)
+      new Item("Item1", 111),
+      new Item("Item2", 222),
+      new Item("Item3", 333)
     ]);
-    this.rightItems = ko.observableArray([]);
+    this.rightItems = ko.observableArray([
+      new Item("Item4", 444)
+    ]);
 
     this.selectedLeft = ko.observableArray([]);
     this.selectedRight = ko.observableArray([]);
@@ -32,11 +34,33 @@ function($, ko) {
   };
 
   ViewModel.prototype.moveLeft = function() {
-    this.move(this.selectedLeft, this.leftItems, this.rightItems);
+    this.move(this.selectedRight, this.rightItems, this.leftItems);
   };
 
   ViewModel.prototype.moveRight = function() {
-    this.move(this.selectedRight, this.rightItems, this.leftItems);
+    this.move(this.selectedLeft, this.leftItems, this.rightItems);
+  };
+
+  ViewModel.prototype.setMoveOne = function(option, item) {
+    var fn = function() {
+      this.moveItemToOther(item);
+    };
+    ko.applyBindingsToNode(option, {event: {dblclick: fn}}, this);
+  }
+
+  ViewModel.prototype.moveItemToOther = function(item) {
+    var existsInLeft = this.leftItems.indexOf(item) >= 0;
+    var existsInRight = this.rightItems.indexOf(item) >= 0;
+
+    if (existsInLeft) {
+      this.leftItems.remove(item);
+      this.rightItems.push(item);
+    }
+
+    if (existsInRight) {
+      this.rightItems.remove(item);
+      this.leftItems.push(item);
+    }
   };
 
   $(document).ready(function() {
